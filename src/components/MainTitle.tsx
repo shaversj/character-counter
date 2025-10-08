0cedc3d65c834466e4e5111f366b09fb1362f825
Josh Shavers <shaversj@gmail.com>
2025-10-08 07:56:59 -0500

    Update h

--- src/components/MainTitle.tsx
+++ src/components/MainTitle.tsx
@@ -6,10 +6,10 @@ export default function MainTitle() {
       <h1
         className={clsx(
           "text-preset-1",
-          "w-[clamp(343px,calc(343px+167*((100vw-375px)/393)),530px)]",
+          "w-[clamp(343px,calc(343px+167*((100vw-375px)/393)),510px)]",
           "text-center",
           "text-[#12131A]",
-          "min-[768px]:w-[510px]",
+          "min-[768px]:w-[530px]",
           "dark:text-neutral-100",
         )}
       >
